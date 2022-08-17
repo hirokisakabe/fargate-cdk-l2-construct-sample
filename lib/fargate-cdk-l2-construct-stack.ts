1fd97518da8119c1d4a2b568212cb7e0d0038e5a
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2022-08-17 21:58:50 +0900

    Add Fargate Stack with L2 Construct

--- lib/fargate-cdk-l2-construct-stack.ts
+++ lib/fargate-cdk-l2-construct-stack.ts
@@ -1,16 +1,51 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from "path";
+import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 
 export class FargateCdkL2ConstructStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "Vpc", { maxAzs: 2 });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FargateCdkL2ConstructQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new ecs.Cluster(this, "EcsCluster", { vpc });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, "TaskDef");
+
+    taskDefinition.addContainer("AppContainer", {
+      image: ecs.ContainerImage.fromAsset(path.resolve(__dirname, "../app")),
+      portMappings: [{ containerPort: 80 }],
+    });
+
+    const service = new ecs.FargateService(this, "FargateService", {
+      cluster,
+      taskDefinition,
+    });
+
+    const loadBalancer = new elbv2.ApplicationLoadBalancer(this, "LB", {
+      vpc,
+      internetFacing: true,
+    });
+
+    const listener = loadBalancer.addListener("PublicListener", {
+      protocol: elbv2.ApplicationProtocol.HTTP,
+    });
+
+    listener.addTargets("ECS", {
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      targets: [
+        service.loadBalancerTarget({
+          containerName: "AppContainer",
+          containerPort: 80,
+        }),
+      ],
+    });
+
+    new CfnOutput(this, "applicationLoadBalancerURL", {
+      value: "http://" + loadBalancer.loadBalancerDnsName,
+    });
   }
 }
