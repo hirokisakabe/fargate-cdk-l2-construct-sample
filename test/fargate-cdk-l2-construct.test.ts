aad7450b9b50d10adbb6fdda6f03f8e996819c01
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2022-08-17 21:40:53 +0900

    cdk init app --language typescript

---
+++ test/fargate-cdk-l2-construct.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FargateCdkL2Construct from '../lib/fargate-cdk-l2-construct-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fargate-cdk-l2-construct-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FargateCdkL2Construct.FargateCdkL2ConstructStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
